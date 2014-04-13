fa7e29817c0b1c507311e3033c7b867be3c370c0
wmmurrah <william.murrah@gmail.com>
2014-04-13 18:05:12 -0400

    added createStarDemoData.R to create new star.csv

---
+++ R/createStarDemoData.R
@@ -0,0 +1,12 @@
+
+
+library(AER)
+
+data(STAR)
+
+library(VIMGUI)
+vmGUImenu()
+star <- STAR[!is.na(STAR$stark), ]
+star <- star[sample(nrow(star), 150,replace=FALSE), ]
+summary(lm(mathk~stark,star))
+write.csv(star, file='data/star.csv')
